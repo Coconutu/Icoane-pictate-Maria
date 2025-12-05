f24ab42e688b7c082aacae64d0106ba4e190fb3b
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-12-05 10:52:19 +0200

    add astronomy picture

---
+++ JavaScript/script-astronomy.js
@@ -0,0 +1,13 @@
+const apiKey = "AIpC1DRu4Sc0k17453q3FxlZFPl15Q66jDL7SlZj";
+const apiLink = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+fetch(apiLink)
+    .then(res=>res.json())
+    .then(data=>{
+        const {date, explanation,hdurl,title}=data;
+        document.getElementById("date").innerText=date;
+        document.getElementById("explanation").innerText=explanation;
+        document.getElementById("hdurl").src=hdurl;
+        document.getElementById("title").innerText=title;
+
+        
+    })
